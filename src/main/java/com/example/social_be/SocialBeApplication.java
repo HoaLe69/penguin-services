de0e418774abc1a450709e5bbe033d0668462cda
Try More <hoabanhanh@gmail.com>
2024-11-16 09:22:29 +0700

    ci: update db connection

--- src/main/java/com/example/social_be/SocialBeApplication.java
+++ src/main/java/com/example/social_be/SocialBeApplication.java
@@ -1,13 +1,8 @@
 package com.example.social_be;
 
-import com.example.social_be.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SocialBeApplication {
