d9feea2717598d87e1ca037314fbf339a48ca59c
Try More <hoabanhanh@gmail.com>
2024-11-16 15:50:33 +0700

    feat: add home page

--- src/main/java/com/example/social_be/config/SecurityConfiguration.java
+++ src/main/java/com/example/social_be/config/SecurityConfiguration.java
@@ -56,6 +56,7 @@ public class SecurityConfiguration {
         .cors(Customizer.withDefaults())
         .authorizeHttpRequests(
             auth -> auth.requestMatchers("/api/auth/**").permitAll()
+                    .requestMatchers("/").permitAll()
                 .requestMatchers("/ws/**").permitAll()
                 .requestMatchers("/api/user/**").authenticated()
                 .requestMatchers("/api/post/**").authenticated()
