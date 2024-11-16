bcf0bc6f9654b2c3a327bdd8d7fbf3c1af112981
Try More <hoabanhanh@gmail.com>
2024-11-16 20:24:38 +0700

    ci: test database connection

--- src/main/java/com/example/social_be/controller/GreetController.java
+++ src/main/java/com/example/social_be/controller/GreetController.java
@@ -1,14 +1,24 @@
 package com.example.social_be.controller;
 
+import com.example.social_be.model.collection.UserCollection;
+import com.example.social_be.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
+import java.util.List;
+
 @Controller
 public class GreetController {
+  @Autowired
+  private UserRepository userRepository;
   @GetMapping("/hi")
   public String greet(Model model) {
+    List<UserCollection> users = userRepository.findAll();
+
     model.addAttribute("message", "Welcome to Penguin!!!");
+    model.addAttribute("users" , users);
     return "home"; // refres to resource/templates/home.html
   }
 }
