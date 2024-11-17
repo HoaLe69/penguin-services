efdd15d8bd7d0ffdf1d02db33209005f5b46bbc4
Try More <hoabanhanh@gmail.com>
2024-11-17 09:43:32 +0700

    ci(cookie): saving tokens

--- src/main/java/com/example/social_be/util/AuthUtil.java
+++ src/main/java/com/example/social_be/util/AuthUtil.java
@@ -8,11 +8,15 @@ public class AuthUtil {
   public void attachTokenInCookieResponse(HttpServletResponse response, String accessToken, String refreshToken) {
     Cookie accessTokenCookie = new Cookie("token", accessToken);
     accessTokenCookie.setHttpOnly(true);
+    accessTokenCookie.setSecure(true);
+    accessTokenCookie.setDomain(".vercel.app");
     accessTokenCookie.setMaxAge(60 * 60 * 24 * 7);
     accessTokenCookie.setPath("/");
 
     Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
     refreshTokenCookie.setHttpOnly(true);
+    refreshTokenCookie.setSecure(true);
+    refreshTokenCookie.setDomain(".vercel.app");
     refreshTokenCookie.setMaxAge(60 * 60 * 24 * 7);
     refreshTokenCookie.setPath("/");
     //
